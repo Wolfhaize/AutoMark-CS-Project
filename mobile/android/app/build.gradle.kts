277ce9b3ec5916b36fcb56db246f7032034ecf1f
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-07-11 17:30:12 +0300

    Updated this file to enable file uploads

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -1,16 +1,14 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
+    id("dev.flutter.flutter-gradle-plugin") // Must come after Android + Kotlin
+    id("com.google.gms.google-services")    // For Firebase
 }
 
 android {
     namespace = "com.patrick.automark"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
-
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -22,11 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.patrick.automark"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -35,8 +30,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -46,7 +39,7 @@ flutter {
     source = "../.."
 }
 
-dependencies{
+dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("com.google.mlkit:text-recognition:16.0.0-beta3")
 }

e61787da16802967c43aa9a3e2d4ded37daa1551
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-07-12 16:29:20 +0300

    Removed error from these files

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -1,13 +1,14 @@
+
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+    id("com.android.application") 
+    id("org.jetbrains.kotlin.android") version "1.8.22" 
     id("dev.flutter.flutter-gradle-plugin") // Must come after Android + Kotlin
-    id("com.google.gms.google-services")    // For Firebase
+    id("com.google.gms.google-services") // For Firebase
 }
 
 android {
     namespace = "com.patrick.automark"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -15,21 +16,19 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
     defaultConfig {
         applicationId = "com.patrick.automark"
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
         multiDexEnabled = true
     }
 
     buildTypes {
         release {
+            isMinifyEnabled = false
+            isShrinkResources = false
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -42,4 +41,12 @@ flutter {
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("com.google.mlkit:text-recognition:16.0.0-beta3")
+}
+
+apply(plugin = "com.google.gms.google-services")
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }

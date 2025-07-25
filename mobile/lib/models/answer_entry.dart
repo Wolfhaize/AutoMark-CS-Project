026f5221d8d71f431e6d01917e7623383ad7de9c
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-07-25 20:34:37 +0300

    Updated this file to ensure a structured marking guide text extrraction

--- mobile/lib/models/answer_entry.dart
+++ mobile/lib/models/answer_entry.dart
@@ -1,7 +1,7 @@
 class AnswerEntry {
-  final String question;
-  final String modelAnswer;
-  final int marks;
+  String question;
+  String modelAnswer;
+  int marks;
 
   AnswerEntry({
     required this.question,
@@ -9,19 +9,15 @@ class AnswerEntry {
     required this.marks,
   });
 
-  factory AnswerEntry.fromJson(Map<String, dynamic> json) {
-    return AnswerEntry(
-      question: json['question'] ?? '',
-      modelAnswer: json['modelAnswer'] ?? '',
-      marks: json['marks'] ?? 0,
-    );
-  }
+  Map<String, dynamic> toJson() => {
+        'question': question,
+        'modelAnswer': modelAnswer,
+        'marks': marks,
+      };
 
-  Map<String, dynamic> toJson() {
-    return {
-      'question': question,
-      'modelAnswer': modelAnswer,
-      'marks': marks,
-    };
-  }
+  factory AnswerEntry.fromJson(Map<String, dynamic> json) => AnswerEntry(
+        question: json['question'] ?? '',
+        modelAnswer: json['modelAnswer'] ?? '',
+        marks: (json['marks'] as num?)?.toInt() ?? 1, // Default fallback
+      );
 }
